20c610bd2e299707dfdefd5d21a3805ebc41867f
PitakaLaxmiVenkataNagaSatya <plvnsatya28@gmail.com>
2025-10-23 11:26:57 +0530

    Initialized project with basic Express server and /health endpoint

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Welcome to Me-API Playground');
+});
+
+app.get('/health', (req, res) => {
+  res.status(200).json({status: 'OK', message: 'Server is running!'});
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
